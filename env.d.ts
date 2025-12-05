659092928071a1216905707b4c63b0888ebe6915
levisantosp <contatowizard01@gmail.com>
2025-12-05 20:17:33 -0300

    chore(lint): change indent rule to 2 spaces

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,11 @@
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            PORT: number
-            HOST: string
-            API_URL: string
-        }
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: number
+      HOST: string
+      API_URL: string
     }
+  }
 }
-export { }
+
+export {}
--- src/config.ts
+++ src/config.ts
@@ -1,187 +1,187 @@
 export const config = {
-    overlay: {
-        base: {
-            role: { left: 0, top: 0 },
-            country: { top: 214, left: 135 },
-            team: { top: 360, left: 120 },
-            stats: {
-                aim: { top: 120, left: -65 },
-                hs: { top: 190, left: -65 },
-                movement: { top: 260, left: -65 },
-                aggression: { top: 120, left: 65 },
-                acs: { top: 190, left: 65 },
-                gamesense: { top: 260, left: 65 }
-            }
-        },
-        'triple crown': {
-            role: { left: 0, top: 30 },
-            country: { top: 244, left: 135 },
-            team: { top: 390, left: 120 },
-            stats: {
-                aim: { top: 120, left: -65 },
-                hs: { top: 190, left: -65 },
-                movement: { top: 260, left: -65 },
-                aggression: { top: 120, left: 65 },
-                acs: { top: 190, left: 65 },
-                gamesense: { top: 260, left: 65 }
-            }
-        },
-        masters: {
-            role: { left: 0, top: 0 },
-            country: { top: 214, left: 135 },
-            team: { top: 360, left: 120 },
-            stats: {
-                aim: { top: 120, left: -65 },
-                hs: { top: 190, left: -65 },
-                movement: { top: 260, left: -65 },
-                aggression: { top: 120, left: 65 },
-                acs: { top: 190, left: 65 },
-                gamesense: { top: 260, left: 65 }
-            }
-        },
-        'valorant masters': {
-            role: { left: -330, top: -200 },
-            country: { top: 564, left: 345 },
-            team: { top: 860, left: 285 },
-            stats: {
-                aim: { top: 220, left: -120 },
-                hs: { top: 350, left: -120 },
-                movement: { top: 480, left: -120 },
-                aggression: { top: 220, left: 120 },
-                acs: { top: 350, left: 120 },
-                gamesense: { top: 480, left: 120 }
-            }
-        },
-        'valorant champions': {
-            role: { left: -330, top: -200 },
-            country: { top: 564, left: 345 },
-            team: { top: 860, left: 285 },
-            stats: {
-                aim: { top: 220, left: -120 },
-                hs: { top: 350, left: -120 },
-                movement: { top: 480, left: -120 },
-                aggression: { top: 220, left: 120 },
-                acs: { top: 350, left: 120 },
-                gamesense: { top: 480, left: 120 }
-            }
-        },
-        champions: {
-            role: { left: 0, top: 0 },
-            country: { top: 214, left: 135 },
-            team: { top: 360, left: 120 },
-            stats: {
-                aim: { top: 120, left: -65 },
-                hs: { top: 190, left: -65 },
-                movement: { top: 260, left: -65 },
-                aggression: { top: 120, left: 65 },
-                acs: { top: 190, left: 65 },
-                gamesense: { top: 260, left: 65 }
-            }
-        },
-        lockin: {
-            role: { left: -330, top: -200 },
-            country: { top: 564, left: 345 },
-            team: { top: 860, left: 285 },
-            stats: {
-                aim: { top: 220, left: -120 },
-                hs: { top: 350, left: -120 },
-                movement: { top: 480, left: -120 },
-                aggression: { top: 220, left: 120 },
-                acs: { top: 350, left: 120 },
-                gamesense: { top: 480, left: 120 }
-            }
-        },
-        // novo overlay "base card" igual ao lockin
-        'base card': {
-            role: { left: -330, top: -200 },
-            country: { top: 564, left: 345 },
-            team: { top: 860, left: 285 },
-            stats: {
-                aim: { top: 220, left: -120 },
-                hs: { top: 350, left: -120 },
-                movement: { top: 480, left: -120 },
-                aggression: { top: 220, left: 120 },
-                acs: { top: 350, left: 120 },
-                gamesense: { top: 480, left: 120 }
-            }
-        }
+  overlay: {
+    base: {
+      role: { left: 0, top: 0 },
+      country: { top: 214, left: 135 },
+      team: { top: 360, left: 120 },
+      stats: {
+        aim: { top: 120, left: -65 },
+        hs: { top: 190, left: -65 },
+        movement: { top: 260, left: -65 },
+        aggression: { top: 120, left: 65 },
+        acs: { top: 190, left: 65 },
+        gamesense: { top: 260, left: 65 }
+      }
     },
-    ovr: {
-        base: { left: -215, top: -280 },
-        'triple crown': { left: -215, top: -250 },
-        masters: { left: -215, top: -280 },
-        champions: { left: -215, top: -280 },
-        'valorant masters': { left: -450, top: -510 },
-        'valorant champions': { left: -450, top: -510 },
-        lockin: { left: -450, top: -510 },
-        'base card': { left: -450, top: -510 }
+    'triple crown': {
+      role: { left: 0, top: 30 },
+      country: { top: 244, left: 135 },
+      team: { top: 390, left: 120 },
+      stats: {
+        aim: { top: 120, left: -65 },
+        hs: { top: 190, left: -65 },
+        movement: { top: 260, left: -65 },
+        aggression: { top: 120, left: 65 },
+        acs: { top: 190, left: 65 },
+        gamesense: { top: 260, left: 65 }
+      }
     },
-    stats: {
-        base: {
-            aim: { left: -210, top: 120 },
-            hs: { left: -180, top: 190 },
-            movement: { left: -210, top: 260 },
-            aggression: { left: 120, top: 120 },
-            acs: { left: 120, top: 190 },
-            gamesense: { left: 120, top: 260 }
-        },
-        'triple crown': {
-            aim: { left: -210, top: 120 },
-            hs: { left: -180, top: 190 },
-            movement: { left: -210, top: 260 },
-            aggression: { left: 120, top: 120 },
-            acs: { left: 120, top: 190 },
-            gamesense: { left: 120, top: 260 }
-        },
-        masters: {
-            aim: { left: -210, top: 120 },
-            hs: { left: -180, top: 190 },
-            movement: { left: -210, top: 260 },
-            aggression: { left: 120, top: 120 },
-            acs: { left: 120, top: 190 },
-            gamesense: { left: 120, top: 260 }
-        },
-        champions: {
-            aim: { left: -210, top: 120 },
-            hs: { left: -180, top: 190 },
-            movement: { left: -210, top: 260 },
-            aggression: { left: 120, top: 120 },
-            acs: { left: 120, top: 190 },
-            gamesense: { left: 120, top: 260 }
-        },
-        'valorant masters': {
-            aim: { left: -410, top: 220 },
-            hs: { left: -410, top: 350 },
-            movement: { left: -410, top: 480 },
-            aggression: { left: 225, top: 220 },
-            acs: { left: 225, top: 350 },
-            gamesense: { left: 225, top: 480 }
-        },
-        'valorant champions': {
-            aim: { left: -410, top: 220 },
-            hs: { left: -410, top: 350 },
-            movement: { left: -410, top: 480 },
-            aggression: { left: 225, top: 220 },
-            acs: { left: 225, top: 350 },
-            gamesense: { left: 225, top: 480 }
-        },
-        lockin: {
-            aim: { left: -410, top: 220 },
-            hs: { left: -410, top: 350 },
-            movement: { left: -410, top: 480 },
-            aggression: { left: 225, top: 220 },
-            acs: { left: 225, top: 350 },
-            gamesense: { left: 225, top: 480 }
-        },
-        'base card': {
-            aim: { left: -410, top: 220 },
-            hs: { left: -410, top: 350 },
-            movement: { left: -410, top: 480 },
-            aggression: { left: 225, top: 220 },
-            acs: { left: 225, top: 350 },
-            gamesense: { left: 225, top: 480 }
-        }
+    masters: {
+      role: { left: 0, top: 0 },
+      country: { top: 214, left: 135 },
+      team: { top: 360, left: 120 },
+      stats: {
+        aim: { top: 120, left: -65 },
+        hs: { top: 190, left: -65 },
+        movement: { top: 260, left: -65 },
+        aggression: { top: 120, left: 65 },
+        acs: { top: 190, left: 65 },
+        gamesense: { top: 260, left: 65 }
+      }
+    },
+    'valorant masters': {
+      role: { left: -330, top: -200 },
+      country: { top: 564, left: 345 },
+      team: { top: 860, left: 285 },
+      stats: {
+        aim: { top: 220, left: -120 },
+        hs: { top: 350, left: -120 },
+        movement: { top: 480, left: -120 },
+        aggression: { top: 220, left: 120 },
+        acs: { top: 350, left: 120 },
+        gamesense: { top: 480, left: 120 }
+      }
+    },
+    'valorant champions': {
+      role: { left: -330, top: -200 },
+      country: { top: 564, left: 345 },
+      team: { top: 860, left: 285 },
+      stats: {
+        aim: { top: 220, left: -120 },
+        hs: { top: 350, left: -120 },
+        movement: { top: 480, left: -120 },
+        aggression: { top: 220, left: 120 },
+        acs: { top: 350, left: 120 },
+        gamesense: { top: 480, left: 120 }
+      }
+    },
+    champions: {
+      role: { left: 0, top: 0 },
+      country: { top: 214, left: 135 },
+      team: { top: 360, left: 120 },
+      stats: {
+        aim: { top: 120, left: -65 },
+        hs: { top: 190, left: -65 },
+        movement: { top: 260, left: -65 },
+        aggression: { top: 120, left: 65 },
+        acs: { top: 190, left: 65 },
+        gamesense: { top: 260, left: 65 }
+      }
+    },
+    lockin: {
+      role: { left: -330, top: -200 },
+      country: { top: 564, left: 345 },
+      team: { top: 860, left: 285 },
+      stats: {
+        aim: { top: 220, left: -120 },
+        hs: { top: 350, left: -120 },
+        movement: { top: 480, left: -120 },
+        aggression: { top: 220, left: 120 },
+        acs: { top: 350, left: 120 },
+        gamesense: { top: 480, left: 120 }
+      }
+    },
+    // novo overlay "base card" igual ao lockin
+    'base card': {
+      role: { left: -330, top: -200 },
+      country: { top: 564, left: 345 },
+      team: { top: 860, left: 285 },
+      stats: {
+        aim: { top: 220, left: -120 },
+        hs: { top: 350, left: -120 },
+        movement: { top: 480, left: -120 },
+        aggression: { top: 220, left: 120 },
+        acs: { top: 350, left: 120 },
+        gamesense: { top: 480, left: 120 }
+      }
     }
+  },
+  ovr: {
+    base: { left: -215, top: -280 },
+    'triple crown': { left: -215, top: -250 },
+    masters: { left: -215, top: -280 },
+    champions: { left: -215, top: -280 },
+    'valorant masters': { left: -450, top: -510 },
+    'valorant champions': { left: -450, top: -510 },
+    lockin: { left: -450, top: -510 },
+    'base card': { left: -450, top: -510 }
+  },
+  stats: {
+    base: {
+      aim: { left: -210, top: 120 },
+      hs: { left: -180, top: 190 },
+      movement: { left: -210, top: 260 },
+      aggression: { left: 120, top: 120 },
+      acs: { left: 120, top: 190 },
+      gamesense: { left: 120, top: 260 }
+    },
+    'triple crown': {
+      aim: { left: -210, top: 120 },
+      hs: { left: -180, top: 190 },
+      movement: { left: -210, top: 260 },
+      aggression: { left: 120, top: 120 },
+      acs: { left: 120, top: 190 },
+      gamesense: { left: 120, top: 260 }
+    },
+    masters: {
+      aim: { left: -210, top: 120 },
+      hs: { left: -180, top: 190 },
+      movement: { left: -210, top: 260 },
+      aggression: { left: 120, top: 120 },
+      acs: { left: 120, top: 190 },
+      gamesense: { left: 120, top: 260 }
+    },
+    champions: {
+      aim: { left: -210, top: 120 },
+      hs: { left: -180, top: 190 },
+      movement: { left: -210, top: 260 },
+      aggression: { left: 120, top: 120 },
+      acs: { left: 120, top: 190 },
+      gamesense: { left: 120, top: 260 }
+    },
+    'valorant masters': {
+      aim: { left: -410, top: 220 },
+      hs: { left: -410, top: 350 },
+      movement: { left: -410, top: 480 },
+      aggression: { left: 225, top: 220 },
+      acs: { left: 225, top: 350 },
+      gamesense: { left: 225, top: 480 }
+    },
+    'valorant champions': {
+      aim: { left: -410, top: 220 },
+      hs: { left: -410, top: 350 },
+      movement: { left: -410, top: 480 },
+      aggression: { left: 225, top: 220 },
+      acs: { left: 225, top: 350 },
+      gamesense: { left: 225, top: 480 }
+    },
+    lockin: {
+      aim: { left: -410, top: 220 },
+      hs: { left: -410, top: 350 },
+      movement: { left: -410, top: 480 },
+      aggression: { left: 225, top: 220 },
+      acs: { left: 225, top: 350 },
+      gamesense: { left: 225, top: 480 }
+    },
+    'base card': {
+      aim: { left: -410, top: 220 },
+      hs: { left: -410, top: 350 },
+      movement: { left: -410, top: 480 },
+      aggression: { left: 225, top: 220 },
+      acs: { left: 225, top: 350 },
+      gamesense: { left: 225, top: 480 }
+    }
+  }
 } as const
 
 export type Key = keyof typeof config.overlay
