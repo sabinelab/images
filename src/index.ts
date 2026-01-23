f780fb40a278896ed8934c9207355b2f674911aa
levisantosp <levi.pires2005@gmail.com>
2026-01-23 07:46:20 -0300

    chore: rename getCard.ts to get-card.ts and move it to src/routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { Elysia } from 'elysia'
-import { getCard } from './scripts/getCard.ts'
+import { getCard } from './routes/get-card'
 
 new Elysia()
   .get('/', { message: 'Hello, world!' })
   .use(getCard)
-  .listen(process.env.PORT)
+  .listen(8000)
 
-console.log(`HTTP server running at ${process.env.PORT}`)
+console.log(`HTTP server running at 8000`)
