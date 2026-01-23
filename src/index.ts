0b4035664e5c37a40f79acfa6d2a52d624fe2c39
levisantosp <levi.pires2005@gmail.com>
2026-01-23 13:29:21 -0300

    feat: add showCard route for dynamic card generation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Elysia } from 'elysia'
 import { getCard } from './routes/get-card'
+import { showCard } from './routes/show-card'
 
 new Elysia()
   .get('/', { message: 'Hello, world!' })
   .use(getCard)
+  .use(showCard)
   .listen(8000)
 
 console.log(`HTTP server running at 8000`)
